1384c49cc6a1b0f69af9988e8c959828e2739ae5
Ronak <32793372+ronakonline@users.noreply.github.com>
2021-12-15 14:36:38 +0530

    article list dynamic completed

--- screens/ArticleList.js
+++ screens/ArticleList.js
@@ -8,9 +8,31 @@ import {
   ArrowBackIcon,
 } from 'native-base';
 import React from 'react';
-import {Text, StyleSheet, Image, TouchableOpacity} from 'react-native';
+import {
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  ActivityIndicator,
+} from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
 
 export default function ArticleList({navigation}) {
+  const [data, setData] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    fetch('https://app.jinjimaharaj.com/api/articles')
+      .then(response => response.json())
+      .then(responseJson => {
+        setData(responseJson);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
   return (
     <>
       <HStack style={styles.header}>
@@ -19,72 +41,43 @@ export default function ArticleList({navigation}) {
           onPress={() => navigation.goBack()}>
           <ArrowBackIcon />
         </TouchableOpacity>
-        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center' }}>
           <Text style={styles.text}>Articles</Text>
         </View>
       </HStack>
+
       <ScrollView style={{flex: 1, backgroundColor: '#FDD8DD'}}>
-        <VStack style={{flex: 1, backgroundColor: '#FDD8DD'}}>
-          <VStack style={styles.content} space={3}>
-            <TouchableOpacity
-              onPress={() => {
-                navigation.navigate('Gallery');
-              }}>
-              <HStack style={styles.video} shadow={7}>
-                <Image
-                  source={{uri: 'https://picsum.photos/200/300'}}
-                  alt="text"
-                  style={styles.videoImage}
-                />
-                <Box style={styles.videoContent}>
-                  <Text style={styles.videoTitle}>Lorem Text Demo</Text>
-                  <Text style={styles.videoDescription}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Pellentesque euismod
-                  </Text>
-                </Box>
-              </HStack>
-            </TouchableOpacity>
-            <TouchableOpacity
-              onPress={() => {
-                navigation.navigate('Gallery');
-              }}>
-              <HStack style={styles.video} shadow={7}>
-                <Image
-                  source={{uri: 'https://picsum.photos/200/300'}}
-                  alt="text"
-                  style={styles.videoImage}
-                />
-                <Box style={styles.videoContent}>
-                  <Text style={styles.videoTitle}>Lorem Text Demo</Text>
-                  <Text style={styles.videoDescription}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Pellentesque euismod
-                  </Text>
-                </Box>
-              </HStack>
-            </TouchableOpacity>
-            <TouchableOpacity
-              onPress={() => {
-                navigation.navigate('Gallery');
-              }}>
-              <HStack style={styles.video} shadow={7}>
-                <Image
-                  source={{uri: 'https://picsum.photos/200/300'}}
-                  alt="text"
-                  style={styles.videoImage}
-                />
-                <Box style={styles.videoContent}>
-                  <Text style={styles.videoTitle}>Lorem Text Demo</Text>
-                  <Text style={styles.videoDescription}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Pellentesque euismod
-                  </Text>
-                </Box>
-              </HStack>
-            </TouchableOpacity>
+        {loading ? (
+          <ActivityIndicator size="large" color="#0000ff" />
+        ) : (
+          <VStack style={{flex: 1, backgroundColor: '#FDD8DD'}}>
+            {data.map((item,index) => (
+            <VStack style={styles.content} key={index} space={3}>
+              <TouchableOpacity
+                onPress={() => {
+                  navigation.navigate('Gallery');
+                }}>
+                <HStack style={styles.video} shadow={7}>
+                  <VStack style={{ flex:1.6, width:'100%', borderRightWidth:1 , padding:5, justifyContent:'center' , alignItems:'center' }}>
+                  <Image
+                    source={require('../images/copywriting.png')}
+                    alt="text"
+                    style={{ width: 80, height:80 }}
+                  />
+                  <Text style={{ fontSize:RFValue(16), fontWeight:'bold', color:'#000' }}>Article {index+1}</Text>
+                  </VStack>
+                  <Box style={styles.videoContent}>
+                    <Text style={styles.videoTitle} adjustsFontSizeToFit numberOfLines={2} >{item.title}</Text>
+                    <Text style={styles.videoDescription} adjustsFontSizeToFit numberOfLines={2.8}>
+                      {item.mini_desc}
+                    </Text>
+                  </Box>
+                </HStack>
+              </TouchableOpacity>
+            </VStack>
+            ))}
           </VStack>
-        </VStack>
+        )}
       </ScrollView>
     </>
   );
@@ -92,7 +85,7 @@ export default function ArticleList({navigation}) {
 
 const styles = StyleSheet.create({
   header: {
-    height:55,
+    height: 55,
     alignItems: 'center',
     backgroundColor: '#FDD8DD',
     borderBottomWidth: 1,
@@ -101,6 +94,7 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 20,
     fontWeight: 'bold',
+    color:'#000',
   },
   heading: {
     height: 50,
@@ -124,9 +118,10 @@ const styles = StyleSheet.create({
   },
   video: {
     width: '100%',
-    height: 120,
+    height: '100%',
     borderRadius: 4,
     backgroundColor: '#FDD3D7',
+    paddingRight:10,
   },
   videoTitle: {
     fontSize: 20,
