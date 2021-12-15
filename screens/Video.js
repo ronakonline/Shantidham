57f1d32e477036cc6119fa8a7b8fadfc4826de1a
Ronak <32793372+ronakonline@users.noreply.github.com>
2021-12-15 16:45:30 +0530

    video dynamic completed

--- screens/Video.js
+++ screens/Video.js
@@ -3,7 +3,9 @@ import {StyleSheet, SafeAreaView, TouchableOpacity} from 'react-native';
 import React from 'react';
 import YoutubePlayer from 'react-native-youtube-iframe';
 
-export default function Video({navigation}) {
+export default function Video({navigation,route}) {
+  const video = route.params.video;
+  const videoId = video.link.split('v=')[1];
   return (
     <View style={styles.container}>
       <HStack style={styles.header}>
@@ -16,20 +18,18 @@ export default function Video({navigation}) {
           <Text style={styles.text}>Video</Text>
         </View>
       </HStack>
-      <View style={styles.title}>
-        <Text style={styles.titleText}>Lorem Ipsem Simple Dummy</Text>
-      </View>
+      {/* <View style={styles.title}>
+        <Text style={styles.titleText}>{video.title}</Text>
+      </View> */}
       <View style={styles.video}>
-        <YoutubePlayer height={220} videoId={'UG5xXxV4I_M'} />
+        <YoutubePlayer height={220} videoId={videoId}  />
       </View>
       <View>
           <View style={styles.title}>
-          <Text style={styles.titleText}>Lorem Ipsem Simple Dummy</Text>
+          <Text style={styles.titleText}>{video.title}</Text>
           </View>
-          <Text style={styles.description}>
-            Lorem ipsum dolor sit amet, cons ectetur adipiscing elit.
-            Pel le ntes que euismod, urna eu tincidunt consectetur, nisi
-            lectus aliquet nunc, eget aliquet nisl
+          <Text style={styles.description} adjustsFontSizeToFit numberOfLines={12}>
+            {video.short_desc}
           </Text>
       </View>
     </View>
--- screens/VideoList.js
+++ screens/VideoList.js
@@ -1,91 +1,75 @@
-import {VStack, Box, HStack, ScrollView,Button,View} from 'native-base';
+import {VStack, Box, HStack, ScrollView, Button, View} from 'native-base';
 import React from 'react';
-import {Text, StyleSheet, Image, TouchableOpacity} from 'react-native';
+import {Text, StyleSheet, Image, TouchableOpacity, Linking,ActivityIndicator} from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
 
 export default function VideoList({navigation}) {
+  const img_url = 'https://app.jinjimaharaj.com/uploads/videolinks/';
+  const [data, setData] = React.useState([]);
+  const [isLoading, setLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    fetch('https://app.jinjimaharaj.com/api/videos')
+      .then(response => response.json())
+      .then(json => {
+        setData(json);
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <>
-      <ScrollView  style={{ flex:1,backgroundColor:'#D4F1DD' }}>
-        <VStack style={{ flex:1,backgroundColor:'#D4F1DD' }}>
-        <Box style={styles.heading}>
-          <Text style={styles.titleText}>Video</Text>
-        </Box>
+      <ScrollView style={{flex: 1, backgroundColor: '#D4F1DD'}}>
+        <VStack style={{flex: 1, backgroundColor: '#D4F1DD'}}>
+          <Box style={styles.heading}>
+            <Text style={styles.titleText}>Video</Text>
+          </Box>
 
-        <VStack style={styles.content} space={3}>
-          
-          <TouchableOpacity onPress={()=>{navigation.navigate('Video')}}>
-          <HStack style={styles.video} shadow={7}>
-            <Box style={styles.videoContent}>
-              <Text style={styles.videoTitle}>Video Title</Text>
-              <Text style={styles.videoDescription}>
-                Video Description Video Description Video Description
-              </Text>
-            </Box>
-            <Image
-              source={{uri: 'https://picsum.photos/200/300'}}
-              alt="text"
-              style={styles.videoImage}
-            />
-          </HStack>
-          </TouchableOpacity>
-          <HStack style={styles.video} shadow={7}>
-            <Box style={styles.videoContent}>
-              <Text style={styles.videoTitle}>Video Title</Text>
-              <Text style={styles.videoDescription}>
-                Video Description Video Description Video Description
-              </Text>
-            </Box>
-            <Image
-              source={{uri: 'https://picsum.photos/200/300'}}
-              alt="text"
-              style={styles.videoImage}
-            />
-          </HStack>
-          <HStack style={styles.video} shadow={7}>
-            <Box style={styles.videoContent}>
-              <Text style={styles.videoTitle}>Video Title</Text>
-              <Text style={styles.videoDescription}>
-                Video Description Video Description Video Description
-              </Text>
-            </Box>
-            <Image
-              source={{uri: 'https://picsum.photos/200/300'}}
-              alt="text"
-              style={styles.videoImage}
-            />
-          </HStack>
-          <HStack style={styles.video} shadow={7}>
-            <Box style={styles.videoContent}>
-              <Text style={styles.videoTitle}>Video Title</Text>
-              <Text style={styles.videoDescription}>
-                Video Description Video Description Video Description
-              </Text>
-            </Box>
-            <Image
-              source={{uri: 'https://picsum.photos/200/300'}}
-              alt="text"
-              style={styles.videoImage}
-            />
-          </HStack>
-          <HStack style={styles.video} shadow={7}>
-            <Box style={styles.videoContent}>
-              <Text style={styles.videoTitle}>Video Title</Text>
-              <Text style={styles.videoDescription}>
-                Video Description Video Description Video Description
-              </Text>
-            </Box>
-            <Image
-              source={{uri: 'https://picsum.photos/200/300'}}
-              alt="text"
-              style={styles.videoImage}
-            />
-          </HStack>
-         
-          
-          <View style={{ flex:1, alignItems:'center', justifyContent:'center', marginTop:20, marginBottom:40  }}>
-              <Button style={{ height:50,width:150,backgroundColor:'#92DCA7' }}><Text style={{ fontSize:18, fontWeight:'bold',color:'#000' }}>View More</Text></Button>
-          </View>
-        </VStack>
+          <VStack style={styles.content} space={3}>
+            {isLoading ? (
+              <ActivityIndicator size="large" color="#0000ff" />
+            ) : (
+              data.map((item, index) => (
+                <TouchableOpacity
+                  key={index}
+                  onPress={() => {
+                    navigation.navigate('Video',{video: item});
+                  }}>
+                  <HStack style={styles.video} shadow={7}>
+                    <Box style={styles.videoContent}>
+                      <Text style={styles.videoTitle} adjustsFontSizeToFit numberOfLines={2} >{item.title}</Text>
+                      <Text style={styles.videoDescription} adjustsFontSizeToFit numberOfLines={2}>
+                        {item.short_desc.substring(0, 30)}...
+                      </Text>
+                    </Box>
+                    <Image
+
+                      source={{uri: img_url + item.img_outer}}
+                      alt="text"
+                      style={styles.videoImage}
+                    />
+                  </HStack>
+                </TouchableOpacity>
+              ))
+            )}
+
+            <View
+              style={{
+                flex: 1,
+                alignItems: 'center',
+                justifyContent: 'center',
+                marginTop: 20,
+                marginBottom: 40,
+              }}>
+              <Button
+                onPress={() => Linking.openURL('https://www.youtube.com/channel/UCTWUaWuTK7GJyJiWbUofm3g')}
+                style={{height: 50, width: 150, backgroundColor: '#92DCA7'}}>
+                <Text style={{fontSize: 18, fontWeight: 'bold', color: '#000'}}>
+                  View More
+                </Text>
+              </Button>
+            </View>
+          </VStack>
         </VStack>
       </ScrollView>
     </>
@@ -101,8 +85,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#cecefb',
     borderBottomWidth: 1,
     borderBottomColor: '#CBEED6',
-    backgroundColor:'#D4F1DD',
-
+    backgroundColor: '#D4F1DD',
   },
   titleText: {
     fontSize: 20,
@@ -111,7 +94,7 @@ const styles = StyleSheet.create({
   },
   content: {
     flex: 1,
-    backgroundColor:'#D4F1DD',
+    backgroundColor: '#D4F1DD',
     padding: 10,
     paddingTop: 15,
   },
@@ -122,7 +105,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#c3eccf',
   },
   videoTitle: {
-    fontSize: 20,
+    fontSize: RFValue(18),
     fontWeight: 'bold',
     marginLeft: 10,
     marginTop: 10,
@@ -140,10 +123,11 @@ const styles = StyleSheet.create({
     height: '100%',
     marginLeft: 10,
     paddingVertical: 10,
+    paddingRight:10,
   },
   videoImage: {
-    flex: 2,
+    flex: 3,
     height: '100%',
-    borderRadius:7,
+    borderRadius: 7,
   },
 });
