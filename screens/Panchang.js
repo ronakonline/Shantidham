ffb4ac4e43fa09d2b8d7afb59dc1b2933f9d97b2
Ronak <32793372+ronakonline@users.noreply.github.com>
2021-12-16 18:22:31 +0530

    Panchang join event after load

--- screens/Panchang.js
+++ screens/Panchang.js
@@ -13,8 +13,6 @@ export default function Panchang({navigation}) {
   var thithiString = '';
   var dayContent = {};
 
- 
-
   React.useEffect(() => {
     fetch('https://app.jinjimaharaj.com/api/panchangs')
       .then(response => response.json())
@@ -51,7 +49,6 @@ export default function Panchang({navigation}) {
         setHeader1(foundData.heading2);
         setHeader2(foundData.heading3);
       }
-    
     } else {
       thithiString = '\n\n';
       dayContent = {
@@ -77,138 +74,144 @@ export default function Panchang({navigation}) {
       {loading ? (
         <ActivityIndicator size="large" color="#0000ff" />
       ) : (
-        <Calendar
-          style={styles.calendar}
-          dayComponent={({date, state}) => {
-            {
-              getdateinfo(date.day, date.month, date.year);
-            }
-            return (
-              <TouchableOpacity
-                onPress={() => {
-                  navigation.navigate('DayPanchang', {date: date.dateString,data:data});
-                }}
-                
-                style={ dayContent.highlight ? styles.markedDate : styles.dateblock}>
-                <VStack
-                  style={{width: 48, overflow: 'hidden', alignSelf: 'center'}}
-                  space={2}>
-                  <View style={styles.dateText}>
-                    <Text style={styles.dateMark}>
-                      {dayContent.event ? '\u2B24' : ''}
-                    </Text>
-                    <Text
-                      style={{
-                        textAlign: 'center',
-                        fontSize: RFValue(14),
-                        fontWeight: 'bold',
-                        color: state === 'disabled' ? 'gray' : '#5C1514',
-                      }}>
-                      {date.day}
-                    </Text>
-                  </View>
+        <>
+          <Calendar
+            style={styles.calendar}
+            dayComponent={({date, state}) => {
+              {
+                getdateinfo(date.day, date.month, date.year);
+              }
+              return (
+                <TouchableOpacity
+                  onPress={() => {
+                    navigation.navigate('DayPanchang', {
+                      date: date.dateString,
+                      data: data,
+                    });
+                  }}
+                  style={
+                    dayContent.highlight ? styles.markedDate : styles.dateblock
+                  }>
+                  <VStack
+                    style={{width: 48, overflow: 'hidden', alignSelf: 'center'}}
+                    space={2}>
+                    <View style={styles.dateText}>
+                      <Text style={styles.dateMark}>
+                        {dayContent.event ? '\u2B24' : ''}
+                      </Text>
+                      <Text
+                        style={{
+                          textAlign: 'center',
+                          fontSize: RFValue(14),
+                          fontWeight: 'bold',
+                          color: state === 'disabled' ? 'gray' : '#5C1514',
+                        }}>
+                        {date.day}
+                      </Text>
+                    </View>
 
-                  <View
-                    style={{
-                      flexDirection: 'row',
-                      justifyContent: 'center',
-                      alignItems: 'center',
-                    }}>
-                    <Text
-                      numberOfLines={2}
-                      adjustsFontSizeToFit
+                    <View
                       style={{
-                        fontSize: RFValue(10),
-                        lineHeight: 14,
-                        fontWeight: 'bold',
-                        color: state === 'disabled' ? 'gray' : '#5C1514',
+                        flexDirection: 'row',
+                        justifyContent: 'center',
+                        alignItems: 'center',
                       }}>
-                      {console.log(date)}
-                      {dayContent.thithi}
-                    </Text>
-                  </View>
-                </VStack>
-              </TouchableOpacity>
-            );
-          }}
-          // Specify theme properties to override specific styles for calendar parts. Default = {}
-          theme={{
-            'stylesheet.calendar.main': {
-              dayContainer: {
-                borderColor: '#DFCBBE',
-                borderWidth: 1,
-                flex: 1,
-                padding: 0,
+                      <Text
+                        numberOfLines={2}
+                        adjustsFontSizeToFit
+                        style={{
+                          fontSize: RFValue(10),
+                          lineHeight: 14,
+                          fontWeight: 'bold',
+                          color: state === 'disabled' ? 'gray' : '#5C1514',
+                        }}>
+                        {console.log(date)}
+                        {dayContent.thithi}
+                      </Text>
+                    </View>
+                  </VStack>
+                </TouchableOpacity>
+              );
+            }}
+            // Specify theme properties to override specific styles for calendar parts. Default = {}
+            theme={{
+              'stylesheet.calendar.main': {
+                dayContainer: {
+                  borderColor: '#DFCBBE',
+                  borderWidth: 1,
+                  flex: 1,
+                  padding: 0,
+                },
+                emptyDayContainer: {
+                  borderColor: '#DFCBBE',
+                  borderWidth: 1,
+                  flex: 1,
+                  padding: 10,
+                },
+                week: {
+                  marginTop: 0,
+                  marginBottom: 0,
+                  flexDirection: 'row',
+                  justifyContent: 'space-around',
+                  color: '#5C1514',
+                },
               },
-              emptyDayContainer: {
-                borderColor: '#DFCBBE',
-                borderWidth: 1,
-                flex: 1,
-                padding: 10,
+              'stylesheet.calendar.header': {
+                dayTextAtIndex0: {
+                  color: '#5C1514',
+                },
+                dayTextAtIndex1: {
+                  color: '#5C1514',
+                },
+                dayTextAtIndex2: {
+                  color: '#5C1514',
+                },
+                dayTextAtIndex3: {
+                  color: '#5C1514',
+                },
+                dayTextAtIndex4: {
+                  color: '#5C1514',
+                },
+                dayTextAtIndex5: {
+                  color: '#5C1514',
+                },
+                dayTextAtIndex6: {
+                  color: '#5C1514',
+                },
               },
-              week: {
-                marginTop: 0,
-                marginBottom: 0,
-                flexDirection: 'row',
-                justifyContent: 'space-around',
+              textSectionTitleColor: '#b6c1cd',
+              textSectionTitleDisabledColor: '#5C1514',
+              selectedDayBackgroundColor: '#5C1514',
+              selectedDayTextColor: '#5C1514',
+              dayTextColor: '#5C1514',
+              textDisabledColor: '#d9e1e8',
+              dotColor: '#00adf5',
+              selectedDotColor: '#FADAC5',
+              arrowColor: 'black',
+              disabledArrowColor: '#d9e1e8',
+              monthTextColor: '#000',
+              indicatorColor: '#000',
+              textDayFontFamily: 'arial',
+              textMonthFontFamily: 'arial',
+              textDayHeaderFontFamily: 'arial',
+              textDayFontWeight: '300',
+              textMonthFontWeight: 'bold',
+              textDayHeaderFontWeight: '300',
+              textDayFontSize: 14,
+              textMonthFontSize: 16,
+              textDayHeaderFontWeight: 'bold',
+              textDayStyle: {
                 color: '#5C1514',
               },
-            },
-            'stylesheet.calendar.header': {
-              dayTextAtIndex0: {
-                color: '#5C1514',
-              },
-              dayTextAtIndex1: {
-                color: '#5C1514',
-              },
-              dayTextAtIndex2: {
-                color: '#5C1514',
-              },
-              dayTextAtIndex3: {
-                color: '#5C1514',
-              },
-              dayTextAtIndex4: {
-                color: '#5C1514',
-              },
-              dayTextAtIndex5: {
-                color: '#5C1514',
-              },
-              dayTextAtIndex6: {
-                color: '#5C1514',
-              },
-            },
-            textSectionTitleColor: '#b6c1cd',
-            textSectionTitleDisabledColor: '#5C1514',
-            selectedDayBackgroundColor: '#5C1514',
-            selectedDayTextColor: '#5C1514',
-            dayTextColor: '#5C1514',
-            textDisabledColor: '#d9e1e8',
-            dotColor: '#00adf5',
-            selectedDotColor: '#FADAC5',
-            arrowColor: 'black',
-            disabledArrowColor: '#d9e1e8',
-            monthTextColor: '#000',
-            indicatorColor: '#000',
-            textDayFontFamily: 'arial',
-            textMonthFontFamily: 'arial',
-            textDayHeaderFontFamily: 'arial',
-            textDayFontWeight: '300',
-            textMonthFontWeight: 'bold',
-            textDayHeaderFontWeight: '300',
-            textDayFontSize: 14,
-            textMonthFontSize: 16,
-            textDayHeaderFontWeight: 'bold',
-            textDayStyle: {
-              color: '#5C1514',
-            },
-            textDayHeaderFontSize: 14,
-          }}
-        />
+              textDayHeaderFontSize: 14,
+            }}
+          />
+          <View style={styles.footer}>
+            <Text style={styles.footerDot}>{'\u2B24'}</Text>
+            <Text style={styles.footerText}>Jain Event</Text>
+          </View>
+        </>
       )}
-      <View style={styles.footer}>
-        <Text style={styles.footerDot}>{'\u2B24'}</Text>
-        <Text style={styles.footerText}>Jain Event</Text>
-      </View>
     </View>
   );
 }
