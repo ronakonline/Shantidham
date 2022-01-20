5e784e3d6e40f56bd40276feb39d44a165d2f369
Ronak <ronak@vicdigit-technologies.com>
2022-01-20 20:26:43 +0530

    donation page form added

--- index.js
+++ index.js
@@ -7,21 +7,26 @@ import App from './App';
 import {name as appName} from './app.json';
 import TrackPlayer from 'react-native-track-player';
 import PushNotification from 'react-native-push-notification';
+import messaging from '@react-native-firebase/messaging';
+
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+  console.log('Message handled in the background!', remoteMessage);
+});
 
 PushNotification.configure({
-    onRegister: function(token) {
-        console.log('TOKEN:', token);
-    },
-    onNotification: function(notification) {
-        console.log('NOTIFICATION:', notification);
-    },
-    permissions: {
-        alert: true,
-        badge: true,
-        sound: true,
-    },
-    popInitialNotification: true,
-    requestPermissions: true,
+  onRegister: function (token) {
+    console.log('TOKEN:', token);
+  },
+  onNotification: function (notification) {
+    console.log('NOTIFICATION:', notification);
+  },
+  permissions: {
+    alert: true,
+    badge: true,
+    sound: true,
+  },
+  popInitialNotification: true,
+  requestPermissions: true,
 });
 
 PushNotification.subscribeToTopic('all');
--- screens/Donation.js
+++ screens/Donation.js
@@ -7,94 +7,175 @@ import {
   TextArea,
   Input,
   Button,
-  View,
-  HStack,
-  ArrowBackIcon
 } from 'native-base';
 import React from 'react';
 import {Text, StyleSheet, TouchableOpacity} from 'react-native';
 
 export default function Donation({navigation}) {
+  function HandleSubmitData() {
+    let data = {
+      type: state.type,
+      description: state.description,
+      name: state.dname,
+      email: state.email,
+      phone: state.phone,
+      amount: state.amount,
+      pan: state.pan,
+      aadhar: state.aadhar,
+    };
+
+    //send data to server
+    fetch('http://abc.com/api/donation', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then(response => response.json())
+      .then(responseJson => {
+        console.log(responseJson);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
+  const handleInputChange = (evt, name) => {
+    const {text} = evt.nativeEvent;
+    setState({
+      ...state,
+      [name]: text,
+    });
+    console.log(state);
+  };
+
+  const [state, setState] = React.useState({
+    type: '',
+    description: '',
+    dname: '',
+    email: '',
+    phone: '',
+    amount: '',
+    pan: '',
+    aadhar: '',
+  });
+
   return (
     <>
-    <HStack style={styles.header}>
-      <TouchableOpacity
-        style={{position: 'absolute'}}
-        onPress={() => navigation.goBack()}>
-        <ArrowBackIcon />
-      </TouchableOpacity>
-      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <Text style={styles.text}>Donation</Text>
-      </View>
-    </HStack>
-    <View style={styles.container}>
-      <Text style={styles.Title}>Coming Soon</Text>
-      <Button style={styles.btn} onPress={() => navigation.navigate('Home')}>
-        <Text style={styles.btnText}>Back</Text>
-      </Button>
-    </View>
-  </>
+      <ScrollView style={{flex: 1, backgroundColor: '#CAE7EF'}}>
+        <VStack style={{flex: 1, backgroundColor: '#CAE7EF'}}>
+          <Box style={styles.heading}>
+            <Text style={styles.titleText}>Donation</Text>
+          </Box>
+
+          <VStack style={styles.content} space={3}>
+            <Select
+              minWidth="200"
+              accessibilityLabel="Select"
+              placeholder="Select"
+              _selectedItem={{
+                bg: '#DCEFF5',
+                backgroundColor: '#DCEFF5',
+                endIcon: <CheckIcon size="5" />,
+              }}
+              mt={1}
+              style={styles.input}>
+              <Select.Item label="UX Research" value="ux" />
+              <Select.Item label="Web Development" value="web" />
+              <Select.Item label="Cross Platform Development" value="cross" />
+              <Select.Item label="UI Designing" value="ui" />
+              <Select.Item label="Backend Development" value="backend" />
+            </Select>
+            <TextArea
+              style={styles.input}
+              placeholder="Description"
+              name="description"
+              value={state.description}
+              onChange={evt =>
+                handleInputChange(evt, 'description')
+              }></TextArea>
+            <Input
+              style={styles.input}
+              placeholder="Name"
+              name="dname"
+              value={state.dname}
+              onChange={evt => handleInputChange(evt, 'dname')}></Input>
+            <Input
+              style={styles.input}
+              placeholder="Email"
+              name="email"
+              value={state.email}
+              onChange={evt => handleInputChange(evt, 'email')}></Input>
+            <Input
+              style={styles.input}
+              placeholder="Phone"
+              name="phone"
+              value={state.phone}
+              onChange={evt => handleInputChange(evt, 'phone')}></Input>
+            <Input
+              style={styles.input}
+              placeholder="Amount"
+              name="amount"
+              value={state.amount}
+              onChange={evt => handleInputChange(evt, 'amount')}></Input>
+            <Input
+              style={styles.input}
+              placeholder="Pan no"
+              name="pan"
+              value={state.pan}
+              onChange={evt => handleInputChange(evt, 'pan')}></Input>
+            <Input
+              style={styles.input}
+              placeholder="Aadhar no"
+              name="aadhar"
+              value={state.aadhar}
+              onChange={evt => handleInputChange(evt, 'aadhar')}></Input>
+            <Button style={styles.button} onPress={HandleSubmitData}>
+              <Text style={styles.buttonText}>Submit</Text>
+            </Button>
+          </VStack>
+        </VStack>
+      </ScrollView>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  header: {
-    height:55,
+  heading: {
+    height: 50,
+    flexDirection: 'row',
     alignItems: 'center',
-    backgroundColor: '#cecefb',
+    justifyContent: 'center',
+    backgroundColor: '#CAE7EF',
     borderBottomWidth: 1,
-    borderBottomColor: '#A8C4E5',
+    borderBottomColor: '#CBEED6',
+    backgroundColor: '#CAE7EF',
   },
-  text: {
+  titleText: {
     fontSize: 20,
     fontWeight: 'bold',
+    color: '#000',
   },
-  container: {
+  content: {
     flex: 1,
-    backgroundColor: '#cecefb',
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 20,
-    paddingBottom:60
+    backgroundColor: '#CAE7EF',
+    padding: 10,
+    paddingTop: 15,
   },
-  Title: {
-    fontSize: 38,
-    fontFamily: 'Arial',
-    color: '#000000',
-    padding: 20,
-    marginBottom:60
+  input: {
+    borderRadius: 0,
+    padding: 10,
+    fontSize: 15,
+    backgroundColor: '#DCEFF5',
   },
-  btn: {
-    backgroundColor: 'purple',
+  button: {
     width: 150,
-    height: 50,
-    borderColor:'#A8C4E5',
-    borderWidth: 2,
-    backgroundColor:'#9E9EF8',
+    backgroundColor: '#7BC4D7',
   },
-  btnText: {
-    fontSize: 20,
-    color: '#ffffff',
+  buttonText: {
+    fontSize: 18,
+    color: '#fff',
   },
 });
-
-// content: {
-//   flex: 1,
-//   backgroundColor: '#CAE7EF',
-//   padding: 10,
-//   paddingTop: 15,
-// },
-// input: {
-//   borderRadius: 0,
-//   padding: 10,
-//   fontSize: 15,
-//   backgroundColor: '#DCEFF5',
-// },
-// button: {
-//   width: 150,
-//   backgroundColor: '#7BC4D7',
-// },
-// buttonText: {
-//   fontSize: 18,
-//   color: '#fff',
-// },
